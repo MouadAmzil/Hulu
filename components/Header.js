913eb17b24a6544108811748d6880f03148c2f51
MouadAmzil <mouadmouad2026@gmail.com>
2022-04-12 02:27:53 +0000

    Add Header Components

---
+++ components/Header.js
@@ -0,0 +1,10 @@
+import Image from 'next/image'
+function Header() {
+    return (
+        <header>
+            <Image src="https://suplari.com/wp-content/uploads/2019/09/hulu-logo-white.png" alt="hulu" width={"100"} height={"50"} />
+        </header>
+    )
+}
+
+export default Header;
--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  images: {
+    domains: ["suplari.com"]
+  },
   reactStrictMode: true,
 }
 
