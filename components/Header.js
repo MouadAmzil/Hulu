10d4f78041b0e884a25fd8c2227e28a6bee41e08
MouadAmzil <mouadmouad2026@gmail.com>
2022-04-12 16:02:24 +0000

    Add Here Icon and headerItme

--- components/Header.js
+++ components/Header.js
@@ -1,10 +1,18 @@
 import Image from 'next/image'
+import HeaderItme from './HeaderItme';
+import { HomeIcon } from "@heroicons/react/outline"
 function Header() {
     return (
         <header>
-            <Image src="https://suplari.com/wp-content/uploads/2019/09/hulu-logo-white.png" alt="hulu" width={"100"} height={"50"} />
+            <HeaderItme Icon={HomeIcon} title={"Home"} />
+            <Image className='object-contain'
+                src="https://suplari.com/wp-content/uploads/2019/09/hulu-logo-white.png"
+                alt="hulu"
+                width={"200"}
+                height={"100"} />
         </header>
     )
 }
 
 export default Header;
+
---
+++ components/HeaderItme.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function HeaderItme({ Icon, title }) {
+    return (
+        <div><Icon /></div>
+    )
+}
+
+export default HeaderItme
