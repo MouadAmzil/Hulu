8db297e08692ed13e17636af4b8826cd0e4c6a2f
MouadAmzil <mouadmouad2026@gmail.com>
2022-04-13 02:36:13 +0000

    Finish nav bar

--- components/Header.js
+++ components/Header.js
@@ -1,15 +1,23 @@
 import Image from 'next/image'
 import HeaderItme from './HeaderItme';
-import { HomeIcon } from "@heroicons/react/outline"
+import { HomeIcon, LightningBoltIcon, UserIcon, SearchIcon, CollectionIcon, BadgeCheckIcon } from "@heroicons/react/outline"
 function Header() {
     return (
-        <header>
-            <HeaderItme Icon={HomeIcon} title={"Home"} />
+        <header className='flex flex-col sm:flex-row m-5 justify-between items-center h-auto'>
+            <div className='flex flex-grow justify-evenly max-w-2xl'>
+                <HeaderItme Icon={HomeIcon} title={"Home"} />
+                <HeaderItme Icon={LightningBoltIcon} title={"Trending"} />
+                <HeaderItme Icon={BadgeCheckIcon} title={"Verified"} />
+                <HeaderItme Icon={CollectionIcon} title={"Collections"} />
+                <HeaderItme Icon={SearchIcon} title={"Serach"} />
+                <HeaderItme Icon={UserIcon} title={"Account"} />
+            </div>
             <Image className='object-contain'
                 src="https://suplari.com/wp-content/uploads/2019/09/hulu-logo-white.png"
                 alt="hulu"
                 width={"200"}
                 height={"100"} />
+
         </header>
     )
 }
--- components/HeaderItme.js
+++ components/HeaderItme.js
@@ -2,7 +2,10 @@ import React from 'react'
 
 function HeaderItme({ Icon, title }) {
     return (
-        <div><Icon /></div>
+        <div className={"flex flex-col items-center cursor-pointer group w-12 sm:w-20 hover"}>
+            <Icon className="h-8 mb-1 group-hover:animate-bounce" />
+            <p className='opacity-0 group-hover:opacity-100 tracking-widest uppercase'>{title}</p>
+        </div>
     )
 }
 
