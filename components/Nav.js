768c14e47836332fc3fb14737afc809ae0aef262
MouadAmzil <mouadmouad2026@gmail.com>
2022-04-13 03:38:38 +0000

    Add Nav categorie of Filme

--- components/Nav.js
+++ components/Nav.js
@@ -2,13 +2,16 @@ import requests from "../utils/requests"
 
 function Nav() {
     return (
-        <>
-            <div>Nav</div>
-            {Object.entries(requests).map(([key, { title, url }]) =>
-                (<h2 key={key}>{title}</h2>)
-            )
-            }
-        </>
+        <nav className="relative">
+            <div className="flex px-10 sm:px-20 whitespace-nowrap text-2xl space-x-6 overflow-x-scroll scrollbar-hide">
+                {Object.entries(requests).map(([key, { title, url }]) =>
+                (<h2 key={key} className="last:pr-10 cursor-pointer transition duration-100 transform hover:scale-125 
+                hover:text-white active:text-red-500">{title}</h2>)
+                )
+                }
+            </div>
+            <div className="absolute top-0 right-0 bg-gradient-to-l from-[#06202A] "></div>
+        </nav>
     )
 }
 
