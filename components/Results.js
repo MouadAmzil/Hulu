32513c3280a03de10f7a0a54301fa4c926091cab
MouadAmzil <mouadmouad2026@gmail.com>
2022-04-17 03:05:22 +0000

    finsih

--- components/Results.js
+++ components/Results.js
@@ -1,11 +1,12 @@
+import FlipMove from 'react-flip-move';
 import Thumbnaid from "./Thumbnaid"
 
 function Results({ results }) {
     return (
-        <div className="px-5 my-10 sm:grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:flex flex-wrap justify-center">
+        <FlipMove className="px-5 my-10 sm:grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:flex flex-wrap justify-center">
             {results.map(res => (
                 <Thumbnaid key={res.id} res={res} />
-            ))}</div>
+            ))}</FlipMove>
     )
 }
 
--- components/Thumbnaid.js
+++ components/Thumbnaid.js
@@ -1,24 +1,30 @@
-import Image from "next/image";
 import { ThumbUpIcon } from "@heroicons/react/outline";
-function Thumbnaid({ res }) {
-    console.log(res)
+import Image from "next/image";
+import { forwardRef } from "react";
+
+const Thumbnail = forwardRef(({ res }, ref) => {
     const BASE_URL = "https://image.tmdb.org/t/p/original/";
     return (
-        <div className="p-2 group cursor-pointer transition duration-200 ease-in transform sm:hover:scale-105 hover:z-50">
+        <div ref={ref} className="p-2 group cursor-pointer transition duration-200 ease-in transform sm:hover:scale-105 hover:z-50">
             <Image
                 layout="responsive"
                 height={1080}
                 width={1920}
+                className='bg-slate-700'
                 src={
                     `${BASE_URL}${res.backdrop_path || res.poster_path}` ||
                     `${BASE_URL}${res.poster_path}`
                 }
-                alt="images"
+                alt={res.title || res.original_name}
+                title={res.title || res.original_name}
             />
             <div className="p-2">
                 <p className="truncate max-w-md">{res.overview}</p>
-                <h2 className="mt-1 truncate max-w-md text-2xl text-white transition-all duration-100 ease-in-out group-hover:font-bold">{res.title || res.original_name}</h2>
-                <p className="flex items-center capitalize opacity-0 group-hover:opacity-100">
+                <h2 className="mt-1 text-2xl truncate max-w-md text-white transition-all duration-100 ease-in-out group-hover:font-bold"
+                >
+                    {res.title || res.original_name}
+                </h2>
+                <p className="flex items-center opacity-0 group-hover:opacity-100">
                     {res.media_type && `${res.media_type} •`}{" "}
                     {res.release_date || res.first_air_date} •{" "}
                     <ThumbUpIcon className="h-5 mx-2" />
@@ -26,7 +32,9 @@ function Thumbnaid({ res }) {
                 </p>
             </div>
         </div>
-    )
-}
+    );
+})
 
-export default Thumbnaid
+Thumbnail.displayName = "Thumbnail";
+
+export default Thumbnail;
